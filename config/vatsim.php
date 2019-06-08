8bb83889cbe2bfd6eeb44af3c9c3c0f90c7e1e6a
Ian Cowan <ian@cowanemail.com>
2019-06-08 17:36:26 -0400

    Working on SSO

--- config/vatsim.php
+++ config/vatsim.php
@@ -4,5 +4,5 @@ return [
     'consumer_key' => env('VATSIM_CONSUMER_KEY'),
     'method' => env('VATSIM_SSO_METHOD', 'RSA-SHA1'),
     'callback_url' => env('VATSIM_SSO_CALLBACK_URL'),
-    'sso_key' => env('VATSIM_SSO_RSA'),
+    'sso_key' => (file_exists(base_path('.sso.rsa')) ? file_get_contents(base_path('.sso.rsa')) : ''),
 ];
