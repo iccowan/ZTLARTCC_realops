570cdb0dd99b5101cbccca6be0cef9842e05b770
Ian Cowan <ian@cowanemail.com>
2019-06-11 15:28:34 +0100

    Pull more flights and transfer flights

---
+++ app/Console/Commands/TransferFlights.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\RwFlight;
+use App\Flight;
+use Illuminate\Console\Command;
+
+class TransferFlights extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Pull:TransferFlights';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Transfers all of the pulled flights';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $flights = Flight::get();
+        foreach($flights as $f) {
+            $newf = new Flight();
+            $newf->callsign = $f->code;
+            $newf->departure = $f->depicao;
+            $newf->arrival = $f->arricao;
+            $newf->flight_plan = $f->route;
+            $newf->dep_time = $f->deptime;
+            $newf->arr_time = $f->arrtime;
+            $newf->save();
+        }
+    }
+}
