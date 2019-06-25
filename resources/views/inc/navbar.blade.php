d21ca7fd58e99aa8d6142dad20a4e98bba68a971
Ian Cowan <ian@cowanemail.com>
2019-06-25 17:51:14 -0400

    Add chart target

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -11,7 +11,7 @@
                     <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://www.aviationapi.com">Charts</a>
+                    <a class="nav-link" href="https://www.aviationapi.com" target="_blank">Charts</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/bookings">Bookings</a>
